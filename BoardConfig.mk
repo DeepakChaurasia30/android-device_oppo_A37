e29dfc791fbe037fdd3dd0d89a9cef1691188350
alienabhishek <abhishekt295@gmail.com>
2018-11-24 00:22:47 +0530

    boot with selinux permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,9 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # LineageHW
 BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
