60466098de24de8f57de88617a4473db41cdeff5
alienabhishek <abhishekt295@gmail.com>
2019-10-03 18:06:07 +0530

    add TARGET_TAP_TO_WAKE_NODE
    also modify hardware/qcom/power/power-helper.c to return true and false instead of 1 and 0.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,7 @@ JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.jav
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Power
-TARGET_POWER_SET_FEATURE_LIB := libpower_set_feature_tomato
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/touchscreen/touchscreen_dev/gesture_ctrl"
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
