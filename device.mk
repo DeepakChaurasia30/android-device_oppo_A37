56b03a2cc6303174229edb9549896e446cfe0710
Brad Ebinger <breadley@google.com>
2019-11-21 20:58:28 +0530

    tomato: Enable FEATURE_TELEPHONY_IMS for these devices
    Bug: 118823723
    Test: checkbuild
    Merged-In: I69fd98160e852b371331a7bc169c7c97dd37f011
    Change-Id: I69fd98160e852b371331a7bc169c7c97dd37f011

--- device.mk
+++ device.mk
@@ -142,7 +142,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
 # Radio
 PRODUCT_COPY_FILES += \
