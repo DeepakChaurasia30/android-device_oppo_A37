d77a240e32965c23efd53f0b5fb56d8726b9416f
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2022-02-05 01:03:12 +0530

    Add lmk props

--- init/init_a37.cpp
+++ init/init_a37.cpp
@@ -33,8 +33,10 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/sysinfo.h>
 
 #include <android-base/file.h>
+#include <android-base/logging.h>
 #include <android-base/properties.h>
 #include <android-base/strings.h>
 
@@ -75,9 +77,28 @@ static void init_alarm_boot_properties()
     }
 }
 
+bool is2GB()
+{
+    struct sysinfo sys;
+    sysinfo(&sys);
+    return sys.totalram > 1024ull * 1024 * 1024;
+}
+
+void set_device_dalvik_properties()
+{
+  property_set("dalvik.vm.heapstartsize", "16m");
+  property_set("dalvik.vm.heapgrowthlimit", is2GB() ? "256m" : "128m");
+  property_set("dalvik.vm.heapsize", is2GB() ? "512m" : "256m");
+  property_set("dalvik.vm.heaptargetutilization", "0.75");
+  property_set("dalvik.vm.heapminfree", is2GB() ? "2m" : "512k");
+  property_set("dalvik.vm.heapmaxfree", "8m");
+  property_set("ro.vendor.qti.sys.fw.bg_apps_limit", is2GB() ? "17" : "9");
+}
+
 void vendor_load_properties()
 {
     // Init a dummy BT MAC address, will be overwritten later
     property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
     init_alarm_boot_properties();
+    set_device_dalvik_properties();
 }
