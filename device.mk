664b9f8862e4153985026a321aa1c312d1acefdf
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2022-02-15 00:54:41 +0530

    ims:discarded tomato ims prop and move new to prop

--- device.mk
+++ device.mk
@@ -107,21 +107,6 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
-# IMS properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    net.lte.volte_call_capable=true \
-    persist.ims.enableADBLogs=1 \
-    persist.ims.enableDebugLogs=1 \
-    persist.radio.calls.on.ims=true \
-    persist.radio.csvt.enabled=false \
-    persist.radio.jbims=1 \
-    persist.radio.ROTATION_ENABLE=1 \
-    persist.radio.VT_ENABLE=1 \
-    persist.radio.VT_HYBRID_ENABLE=1 \
-    persist.radio.VT_USE_MDM_TIME=0 \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.volte_enabled_by_hw=1
-
 PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
     ril.subscription.types=NV,RUIM
@@ -182,6 +167,7 @@ PRODUCT_PACKAGES += \
 # Telephony packages
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    
 # msm-irq
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
